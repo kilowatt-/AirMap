9719cc9b1941d94c48816335ec0f0ecc8b93bd31
Wong Kean Wah <2560596+kilowatt-@users.noreply.github.com>
2020-04-06 21:24:14 -0700

    changed npm script for deploy

--- server/index.js
+++ server/index.js
@@ -46,4 +46,12 @@ app.get('/routeData/:airportCode', async (req, res) => {
 
 });
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
